c5e2463a417b8d50b7e09389ee118bf3ff1f152d
Srijan Kundu <srijan.kundu7@gmail.com>
2022-08-24 01:59:28 +0530

    Aug 24, 2022

---
+++ Classworks/Aug, '22/23/binsrch.py
@@ -0,0 +1,23 @@
+def bin_search(arr, temp):
+    low = 0
+    high = len(arr)
+
+    while low <= high:
+        m = (low+high)//2
+        if temp == arr[m]:
+            print("Value found at index", m, ".")
+            break
+        else:
+            if temp > arr[m]:
+                low = m+1
+            else:
+                high = m-1
+    if low>high:
+        print("Value not found!")
+
+
+if __name__ == '__main__':
+    arr = [5, 9, 10, 17, 23, 31, 36, 37, 99]   # sorting of the list is to be incorporated later on
+    temp = 10                                  # take this as user-input after the aforesaid incorporation
+
+    bin_search(arr, temp)
