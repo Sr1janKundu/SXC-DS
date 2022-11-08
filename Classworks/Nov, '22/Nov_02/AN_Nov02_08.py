5198d9f67a9528f06281c0a5ae26f5b1615ce617
Srijan Kundu <srijan.kundu7@gmail.com>
2022-11-09 00:12:59 +0530

    Nov 09, 2022

--- Classworks/Nov, '22/Nov_02/AN_Nov02_08.py
+++ Classworks/Nov, '22/Nov_02/AN_Nov02_08.py
@@ -1,3 +1,17 @@
 '''
 Write a program to extract words from any text file and store in another file. Display all words and also number of words in that file.
 '''
+
+file1 = input('Enter input file name: ')
+
+fp1 = open(file1, 'rb')
+
+count = 0
+for line in fp1:
+    for word in line.split():
+        print(word)
+        count += 1
+
+print(f"There are {count} words in the file '{file1}'.")
+
+fp1.close()
